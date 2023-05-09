ce9947441e88acdc3ff569baee37e366413fc6b0
withsme <tmdal9953@gmail.com>
2023-05-09 16:22:02 +0900

    Design 홈페이지 및 라우팅 설정

--- src/App.js
+++ src/App.js
@@ -1,10 +1,23 @@
-import React from "react";
+import React from 'react';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import './App.css';
+import Home from './Pages/Home/Home.js'
+import Make from './Pages/Meeting/Make.js'
+import Board from './Pages/Meeting/Board.js'
+import Check from './Pages/Restaurant/Check.js'
+import NotFound from './Pages/NotFound.js';
 
 function App() {
   return (
-    <div>
-      Success! 
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />}></Route>
+        <Route path="/make" element={<Make />}></Route>
+        <Route path="/check" element={<Check />}></Route>
+        <Route path="/board" element={<Board />}></Route>
+        <Route path="*" element={<NotFound />}></Route>
+      </Routes>
+    </BrowserRouter>
   );
 }
 
